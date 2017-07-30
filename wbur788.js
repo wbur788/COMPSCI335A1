aa69bd9e3347ac78bb6d9b21c119124005088b8f
wbur788 <wbur788@aucklanduni.ac.nz>
2017-07-31 04:21:03 +1200

    Sussed out a bit of the aesthetics, and changed the books layout

--- wbur788.js
+++ wbur788.js
@@ -6,22 +6,24 @@ function getBooks() {
     xhr.onload = function () {
         var resp = JSON.parse(xhr.responseText);
         showBooks(resp);
-        alert(xhr.responseText)
     }
     xhr.send(null);
 }
 
 function showBooks(books) {
-    var tableContent = "<tr class='orderTitle'><td>Author</td><td>Title</td></tr>\n";
-    for (var i = 0; i < books.length; ++i){
+    var tableContent = "";
+    for (var i = 0; i < books.length-3; i+=4){
         var record = books[i];
+        var record2 = books[i+1];
+        var record3 = books[i+2];
+        var record4 = books[i+3];
         if (i & 1 == 1) {
             tableContent += "<tr class='orderOdd'>";
         }
         else {
             tableContent += "<tr class='orderEven'>";
         }
-        tableContent += "<td><img src='http://redsox.uoa.auckland.ac.nz/BC/Open/Service.svc/bookimg?id=" + record.Id + "'/></td><td>" + record.AuthorInitials + " " + record.AuthorSurname + "</td><td>" + record.Title + "</td></tr>\n";
+        tableContent += "<td><img src='http://redsox.uoa.auckland.ac.nz/BC/Open/Service.svc/bookimg?id=" + record.Id + "'/><figcaption>" + record.AuthorInitials + " " + record.AuthorSurname + "</figcaption><figcaption>"+ record.Title + "</figcaption></td><td><img src='http://redsox.uoa.auckland.ac.nz/BC/Open/Service.svc/bookimg?id=" + record2.Id + "'/><figcaption>" + record2.AuthorInitials + " " + record2.AuthorSurname + "</figcaption><figcaption>"+ record2.Title + "</figcaption></td><td><img src='http://redsox.uoa.auckland.ac.nz/BC/Open/Service.svc/bookimg?id=" + record3.Id + "'/><figcaption>" + record3.AuthorInitials + " " + record3.AuthorSurname + "</figcaption><figcaption>"+ record3.Title + "</figcaption></td><td><img src='http://redsox.uoa.auckland.ac.nz/BC/Open/Service.svc/bookimg?id=" + record4.Id + "'/><figcaption>" + record4.AuthorInitials + " " + record4.AuthorSurname + "</figcaption><figcaption>"+ record4.Title + "</figcaption></td></tr>\n";
     } 
     document.getElementById("showTab").innerHTML = tableContent;
 }
