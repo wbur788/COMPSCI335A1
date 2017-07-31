df8daea81fedb841e9d43083572808804d327ec6
wbur788 <wbur788@aucklanduni.ac.nz>
2017-07-31 14:39:06 +1200

    Sussed out the br stuff

--- wbur788.js
+++ wbur788.js
@@ -28,3 +28,32 @@ function showBooks(books) {
     document.getElementById("showTab").innerHTML = tableContent;
 }
 
+function getBr() {
+    var uri = "http://redsox.uoa.auckland.ac.nz/BC/Open/Service.svc/brlist";
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", uri, true);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onload = function () {
+        var resp = JSON.parse(xhr.responseText);
+        showBr(resp);
+    }
+    xhr.send(null);
+}
+
+function showBr(br) {
+    var tableContent = "";
+    for (var i = 0; i < br.length-3; i+=4){
+        var record = br[i];
+        var record2 = br[i+1];
+        var record3 = br[i+2];
+        var record4 = br[i+3];
+        if (i & 1 == 1) {
+            tableContent += "<tr class='orderOdd'>";
+        }
+        else {
+            tableContent += "<tr class='orderEven'>";
+        }
+        tableContent += "<td><img src='http://redsox.uoa.auckland.ac.nz/BC/Open/Service.svc/brimg?id=" + record.Id + "'/><figcaption>"+ record.Title + "</figcaption></td><td><img src='http://redsox.uoa.auckland.ac.nz/BC/Open/Service.svc/brimg?id=" + record2.Id + "'/><figcaption>"+ record2.Title + "</figcaption></td><td><img src='http://redsox.uoa.auckland.ac.nz/BC/Open/Service.svc/brimg?id=" + record3.Id + "'/><figcaption>"+ record3.Title + "</figcaption></td><td><img src='http://redsox.uoa.auckland.ac.nz/BC/Open/Service.svc/brimg?id=" + record4.Id + "'/><figcaption>"+ record4.Title + "</figcaption></td></tr>\n";
+    } 
+    document.getElementById("showTab").innerHTML = tableContent;
+}
